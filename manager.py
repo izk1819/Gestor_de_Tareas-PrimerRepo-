b70c068861ad0743201abb64f7d25e5f3d544e47
izk1819 <izaak.1819@gmail.com>
2025-10-26 02:45:33 -0300

    Add files via upload
    -Se crea una tabla de usuarios en la base de datos
    -Se comienza a usar Flask
    -"Maquetas" de primeros dos archivos .HTML

--- manager.py
+++ manager.py
@@ -28,5 +28,42 @@ def query(task_list):
 # Cargar datos para MySQL:
 load_dotenv()
 
-# Si no exixte, crear la base de datos:
-query(["create database if not exists task_manager"])
+# Si no exixte, crear la base de datos junto con la tabla de usuarios:
+query(["create database if not exists task_manager",
+       "use task_manager",
+       """create table if not exists users (
+       id int auto_increment primary key,
+       name varchar(50) not null,
+       email varchar(50) not null unique,
+       password varchar(50) not null,
+       created_at timestamp default current_timestamp
+       )"""])
+
+
+
+app = Flask(__name__)
+# Definir páginas:
+@app.route("/")
+def task_manager():
+    return render_template("task_manager.html")
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    return render_template("users/login.html")
+
+@app.route("/sign_up", methods = ["GET","POST"])
+def sign_up():
+
+    if request.method == "POST":
+        ####################################
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        #ENVIAR A LA BASE DE DATOS
+        #####################################
+
+    return render_template("users/sign_up.html")
+
+# Iniciar Flask
+if __name__ == '__main__':
+    app.run(debug=True)
